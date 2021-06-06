94bb2c5661741a95654055f702a0913dc4c88edd
Victor André Gris Costa <victorandr98@gmail.com>
2021-06-05 22:23:47 -0300

    Add under construction message

---
+++ src/ConstructionMessage.svelte
@@ -0,0 +1,36 @@
+<main>
+	<h1>Hello!</h1>
+	<h2>This website is under construction</h2>
+	<p>Made with <a href="https://svelte.dev">Svelte</a></p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2 {
+    color: #333;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+  a {
+    color: #ff3e00;
+  }
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import App from './App.svelte';
+import ConstructionMessage from './ConstructionMessage.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+const underConstruction = true;
+
+let app;
+if (underConstruction) {
+	app = new ConstructionMessage({
+		target: document.body,
+	});
+} else {
+	app = new App({
+		target: document.body,
+		props: {
+			name: 'world'
+		}
+	});
+}
 
 export default app;
